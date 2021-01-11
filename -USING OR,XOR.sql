bcc6513a8a85052bbb845c6bd8b981d5ed537461
Mukesh Hunasekatte Nagabhushan <65340618+mukeshhnagabhushan@users.noreply.github.com>
2021-01-11 21:01:57 +0000

    using OR,XOR

---
+++ -USING OR,XOR.sql
@@ -0,0 +1,12 @@
+--Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE area>3000000 OR population >250000000;
+
+--Exclusive OR (XOR). Show the countries that
+--are big by area (more than 3 million) or
+--big by population (more than 250 million) but not both.
+--Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE area>3000000 xor population >250000000
